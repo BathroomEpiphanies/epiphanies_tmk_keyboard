81376c2efd6b612fb77f6fc8051dae93931bda62
Fredrik Atmer <epiphanies@atmer.se>
2016-07-09 11:00:16 +0200

    Updated tmk_core

--- keyboard/sane_matrix_20151027/keymap_ansi.h
+++ keyboard/sane_matrix_20151027/keymap_ansi.h
@@ -2,20 +2,20 @@
 static const uint8_t PROGMEM keymaps[][MATRIX_COLS][MATRIX_ROWS] = {
     /* Layer 0: Standard ANSI layer */
     KEYMAP(\
-         ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,  F12,        PSCR,SLCK,PAUS,                         \
-         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL,BSPC,   INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS, \
-         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,   DEL, END, PGDN,    P7,  P8,  P9,  PPLS, \
-         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,      ENT,                      P4,  P5,  P6,        \
-         LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,        UP,           P1,  P2,  P3,  PENT, \
-         LCTL,LGUI,LALT,               SPC,                RALT,RGUI, FN0,RCTL,   LEFT,DOWN,RGHT,    P0,  PDOT),          \
+         ESC,     F1,  F2,  F3,  F4,     F5,  F6,  F7,  F8,     F9, F10, F11, F12,    PSCR,SLCK,PAUS,                          \
+         GRV, NO,  1,   2,   3,   4,   5,  NO,    6,   7,   8,   9,   0,MINS, EQL,     INS,HOME,PGUP,   NLCK,PSLS,PAST,PMNS,   \
+         TAB,    Q,   W,   E,   R,   T,BSLS,RBRC,   Y,   U,   I,   O,   P,   BSPC,     DEL, END,PGDN,     P7,  P8,  P9,  PPLS, \
+         CAPS,  A,   S,   D,   F,   G,LBRC,  QUOT,   H,   J,   K,   L,SCLN,   ENT,                        P4,  P5,  P6,        \
+         LSFT,       Z,   X,   C,   V,   B,NUBS,SLSH, N,   M,COMM, DOT,      RSFT,           UP,          P1,  P2,  P3,  PENT, \
+         LCTL,  FN0,LGUI, LALT,     BSPC,   SPC,   ENT,     RALT, RGUI,MENU, RCTL,    LEFT,DOWN,RGHT,     P0,     PDOT),       \
     /* Layer 1: Function layer */
     KEYMAP(\
-         CALC,MYCM,WSCH,WHOM,MAIL,VOLD,VOLU,MSEL,MSTP,MPLY,MPRV,MNXT,TRNS,        WAKE, PWR,SLEP,                         \
-         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
-         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
-         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,                      TRNS,TRNS,TRNS,      \
-         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,        TRNS,         TRNS,TRNS,TRNS,TRNS, \
-         TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS)
+         CALC,     MYCM,WSCH,WHOM,MAIL,  VOLD,VOLU,MSEL,MSTP,   MPLY,MPRV,MNXT,TRNS,   WAKE, PWR,SLEP,                         \
+         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
+         TRNS,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
+         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  TRNS,                      TRNS,TRNS,TRNS,      \
+         TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,        TRNS,         TRNS,TRNS,TRNS,TRNS, \
+         TRNS, TRNS,TRNS, TRNS,      TRNS  ,TRNS,  TRNS,      TRNS, TRNS,TRNS, TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS)
 };
 
 static const uint16_t PROGMEM fn_actions[] = {
--- keyboard/sane_matrix_20151027/matrix.c
+++ keyboard/sane_matrix_20151027/matrix.c
@@ -1,18 +1,18 @@
 /*
-Copyright 2014 Ralf Schmitt <ralf@bunkertor.net>
+  Copyright 2014 Ralf Schmitt <ralf@bunkertor.net>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdint.h>
@@ -33,211 +33,135 @@ static matrix_row_t matrix[MATRIX_ROWS];
 static matrix_row_t matrix_debouncing[MATRIX_ROWS];
 
 static uint8_t read_rows(void);
-static void init_rows(void);
-static void unselect_cols(void);
 static void select_col(uint8_t col);
 
-inline uint8_t matrix_rows(void)
-{
-    return MATRIX_ROWS;
+inline uint8_t matrix_rows(void) {
+  return MATRIX_ROWS;
 }
 
-inline uint8_t matrix_cols(void)
-{
-    return MATRIX_COLS;
-}
-
-void matrix_init(void)
-{
-    unselect_cols();
-    init_rows();
-    for (uint8_t i=0; i < MATRIX_ROWS; i++)  {
-        matrix[i] = 0;
-        matrix_debouncing[i] = 0;
-    }
-}
-
-uint8_t matrix_scan(void)
-{
-    for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-        select_col(col);
-        _delay_us(3);
-        uint8_t rows = read_rows();
-        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-            bool prev_bit = matrix_debouncing[row] & ((matrix_row_t)1<<col);
-            bool curr_bit = rows & (1<<row);
-            if (prev_bit != curr_bit) {
-                matrix_debouncing[row] ^= ((matrix_row_t)1<<col);
-                debouncing = DEBOUNCE;
-            }
-        }
-        unselect_cols();
-    }
-
-    if (debouncing) {
-        if (--debouncing) {
-            _delay_ms(1);
-        } else {
-            for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-                matrix[i] = matrix_debouncing[i];
-            }
-        }
-    }
-
-    return 1;
-}
-
-bool matrix_is_modified(void)
-{
-    if (debouncing) return false;
-    return true;
-}
-
-inline bool matrix_is_on(uint8_t row, uint8_t col)
-{
-    return (matrix[row] & ((matrix_row_t)1<<col));
-}
-
-inline matrix_row_t matrix_get_row(uint8_t row)
-{
-    return matrix[row];
-}
-
-void matrix_print(void)
-{
-    print("\nr/c 0123456789ABCDEF\n");
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        xprintf("%02X: %032lb\n", row, bitrev32(matrix_get_row(row)));
-    }
-}
-
-uint8_t matrix_key_count(void)
-{
-    uint8_t count = 0;
-    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-        count += bitpop32(matrix[i]);
-    }
-    return count;
+inline uint8_t matrix_cols(void) {
+  return MATRIX_COLS;
 }
 
 /* Row pin configuration
  *
  * row: 0    1    2    3    4    5    6    7
- * pin: PC7  PB5  PB4  PB6  PB1  PB0  PB3  PB2
+ * pin: PB4  PB6  PC2  PC4  PC7  PD0  PD1  PD4
  *
+ * Column pin congiguration
+ * 
+ * These columns uses three 74HC138 3 to 8 bit demultiplexers (low
+ * active) to control the 18 columns. The columns are driven from pins
+ * B5,3,2,1,0.
  */
-static void init_rows(void)
-{
-    DDRC &= ~0b10000000;
-    DDRB &= ~0b01111111;
-    PORTC |= 0b10000000;
-    PORTB |= 0b01111111;
+void matrix_init(void) {
+  DDRB  &= ~0b01010000;  // Row input pins
+  DDRC  &= ~0b10010100;
+  DDRD  &= ~0b01001010;
+  PORTB |=  0b01010000;
+  PORTC |=  0b10010100;
+  PORTD |=  0b01001010;
+
+  DDRB  |=  0b00101111;  // Column output pins
+
+  for (uint8_t i=0; i < MATRIX_ROWS; i++)  {
+    matrix[i] = 0;
+    matrix_debouncing[i] = 0;
+  }
 }
 
-static uint8_t read_rows(void)
-{
-    return (PINC&(1<<7) ? 0 : (1<<0)) |
-           (PINB&(1<<5) ? 0 : (1<<1)) |
-           (PINB&(1<<4) ? 0 : (1<<2)) |
-           (PINB&(1<<6) ? 0 : (1<<3)) |
-           (PINB&(1<<1) ? 0 : (1<<4)) |
-           (PINB&(1<<0) ? 0 : (1<<5)) |
-           (PINB&(1<<3) ? 0 : (1<<6)) |
-           (PINB&(1<<2) ? 0 : (1<<7));
-}
-
-/*  These columns uses two 74HC42 4 to 10 bit demultiplexers (low active).
- *
- *  TODO: These definitions do not correspond to the code below. They
- *  have only been copied from the code for the Kitten Paw. They need
- *  to be sorted out when someone has the time...
- *
- *   COL PD1 PD0 PD2 PD6 PD5 PD4
- *   12   1   1   0   0   0   0
- *   11   1   1   0   0   0   1
- *   10   1   1   0   0   1   0
- *    9   1   1   0   0   1   1
- *    8   1   1   0   1   0   0
- *    7   1   1   0   1   0   1
- *    6   1   1   0   1   1   0
- *    5   1   1   0   1   1   1
- *    4   1   1   1   0   0   0
- *    3   1   1   1   0   0   1
-
- *   COL PD2 PD6 PD1 PD0 PD5 PD4
- *    2   1   1   0   0   0   0
- *    1   1   1   0   0   0   1
- *    0   1   1   0   0   1   0
- *   17   1   1   0   0   1   1
- *   16   1   1   0   1   0   0
- *        1   1   0   1   0   1
- *        1   1   0   1   1   0
- *   15   1   1   0   1   1   1
- *   14   1   1   1   0   0   0
- *   13   1   1   1   0   0   1
- */
-static void unselect_cols(void)
-{
-    DDRD |= 0b01111011;
-    PORTD &= ~0b01111011;
-}
-
-static void select_col(uint8_t col)
-{
-    switch (col) {
-        case 0:
-            PORTD |= (1<<5) | (1<<1) | (1<<0);
-            break;
-        case 1:
-            PORTD |= (1<<6) | (1<<5) | (1<<4) | (1<<3);
-            break;
-        case 2:
-            PORTD |= (1<<4) | (1<<1) | (1<<0);
-            break;
-        case 3:
-            PORTD |= (1<<6) | (1<<5) | (1<<3) | (1<<0);
-            break;
-        case 4:
-            PORTD |= (1<<3) | (1<<1) | (1<<0);
-            break;
-        case 5:
-            PORTD |= (1<<5) | (1<<4) | (1<<3) | (1<<1) | (1<<0);
-            break;
-        case 6:
-            PORTD |= (1<<6) | (1<<5) | (1<<3);
-            break;
-        case 7:
-            PORTD |= (1<<6) | (1<<5);
-            break;
-        case 8:
-            PORTD |= (1<<6) | (1<<5) | (1<<4);
-            break;
-        case 9:
-            PORTD |= (1<<6) | (1<<5) | (1<<0);
-            break;
-        case 10:
-            PORTD |= (1<<6) | (1<<5) | (1<<4) | (1<<0);
-            break;
-        case 11:
-            PORTD |= (1<<6) | (1<<5) | (1<<1);
-            break;
-        case 12:
-            PORTD |= (1<<6) | (1<<5) | (1<<4) | (1<<3) | (1<<0);
-            break;
-        case 13:
-            PORTD |= (1<<6) | (1<<5) | (1<<3) | (1<<1);
-            break;
-        case 14:
-            PORTD |= (1<<6) | (1<<1) | (1<<0);
-            break;
-        case 15:
-            PORTD |= (1<<4) | (1<<3) | (1<<1) | (1<<0);
-            break;
-        case 16:
-            PORTD |= (1<<5) | (1<<3) | (1<<1) | (1<<0);
-            break;
-        case 17:
-            PORTD |= (1<<5) | (1<<4) | (1<<1) | (1<<0);
-            break;
+uint8_t matrix_scan(void) {
+  for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+    select_col(col);
+    _delay_us(3);
+    uint8_t rows = read_rows();
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+      bool prev_bit = matrix_debouncing[row] & ((matrix_row_t)1<<col);
+      bool curr_bit = rows & (1<<row);
+      if (prev_bit != curr_bit) {
+        matrix_debouncing[row] ^= ((matrix_row_t)1<<col);
+        debouncing = DEBOUNCE;
+      }
     }
+  }
+
+  if (debouncing) {
+    if (--debouncing) {
+      _delay_ms(1);
+    }
+    else {
+      for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        matrix[i] = matrix_debouncing[i];
+      }
+    }
+  }
+
+  return 1;
+}
+
+bool matrix_is_modified(void) {
+  if (debouncing)
+    return false;
+  else
+    return true;
+}
+
+inline bool matrix_is_on(uint8_t row, uint8_t col) {
+  return (matrix[row] & ((matrix_row_t)1<<col));
+}
+
+inline matrix_row_t matrix_get_row(uint8_t row) {
+  return matrix[row];
+}
+
+void matrix_print(void) {
+  print("\nr/c 0123456789ABCDEF\n");
+  for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+    xprintf("%02X: %032lb\n", row, bitrev32(matrix_get_row(row)));
+  }
+}
+
+uint8_t matrix_key_count(void) {
+  uint8_t count = 0;
+  for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+    count += bitpop32(matrix[i]);
+  }
+  return count;
+}
+
+static uint8_t read_rows(void) {
+  return
+    (PINC&(1<<2) ? 0 : (1<<0)) |
+    (PIND&(1<<1) ? 0 : (1<<1)) |
+    (PIND&(1<<4) ? 0 : (1<<2)) |
+    (PINB&(1<<4) ? 0 : (1<<3)) |
+    (PINB&(1<<6) ? 0 : (1<<4)) |
+    (PINC&(1<<7) ? 0 : (1<<5));
+}
+
+static void select_col(uint8_t col) {
+  switch (col) {
+  case  0: PORTB = (PORTB & ~0b00101111) | 0b00001111; break;
+  case  1: PORTB = (PORTB & ~0b00101111) | 0b00000011; break;
+  case  2: PORTB = (PORTB & ~0b00101111) | 0b00000000; break;
+  case  3: PORTB = (PORTB & ~0b00101111) | 0b00101010; break;
+  case  4: PORTB = (PORTB & ~0b00101111) | 0b00000100; break;
+  case  5: PORTB = (PORTB & ~0b00101111) | 0b00101110; break;
+  case  6: PORTB = (PORTB & ~0b00101111) | 0b00001000; break;
+  case  7: PORTB = (PORTB & ~0b00101111) | 0b00101100; break;
+  case  8: PORTB = (PORTB & ~0b00101111) | 0b00101000; break;
+  case  9: PORTB = (PORTB & ~0b00101111) | 0b00001100; break;
+  case 10: PORTB = (PORTB & ~0b00101111) | 0b00100100; break;
+  case 11: PORTB = (PORTB & ~0b00101111) | 0b00100000; break;
+  case 12: PORTB = (PORTB & ~0b00101111) | 0b00100010; break;
+  case 13: PORTB = (PORTB & ~0b00101111) | 0b00000001; break;
+  case 14: PORTB = (PORTB & ~0b00101111) | 0b00100110; break;
+  case 15: PORTB = (PORTB & ~0b00101111) | 0b00001001; break;
+  case 16: PORTB = (PORTB & ~0b00101111) | 0b00001110; break;
+  case 17: PORTB = (PORTB & ~0b00101111) | 0b00001101; break;
+  case 18: PORTB = (PORTB & ~0b00101111) | 0b00001011; break;
+  case 19: PORTB = (PORTB & ~0b00101111) | 0b00001010; break;
+  case 20: PORTB = (PORTB & ~0b00101111) | 0b00000110; break;
+  case 21: PORTB = (PORTB & ~0b00101111) | 0b00000010; break;
+  }
 }
